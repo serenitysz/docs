83a90b06dfb4c03042d37018b597e3f0e9698075
Victor Albuquerque <albq.victor@gmail.com>
2026-01-05 15:07:49 -0300

    chore: change project metadata and solve build bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
-  base: command === "build" ? "/serenity/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
